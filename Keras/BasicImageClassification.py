fbbc3a79f087f456f08c9f780de692d61c97a0ac
shubowei <shubowei@changingedu.com>
2020-09-18 18:23:43 +0800

    Add BasicImageClassification Case

--- Keras/BasicImageClassification.py
+++ Keras/BasicImageClassification.py
@@ -1,7 +1,8 @@
+# Ref:https://www.tensorflow.org/tutorials/keras/classification
+
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
